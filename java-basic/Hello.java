ccef8f2b6e9b47907ee492b26861a3c53de74969
gkry21 <gkry123@naver.com>
2019-08-09 18:12:44 +0900

    190809 18:12

--- java-basic/Hello.java
+++ java-basic/Hello.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
 class Hello{
     public static void main(String[] args){
-        System.out.print("Hello !!");
+        System.out.println("이름 뭐니 ?");
+        String name = "";
+        //값을 입력받는 지점
+        Scanner scanner = new Scanner(System.in);
+        name = scanner.next();
+        System.out.println("이름은 " + name + "이다");
+        System.out.println("몇살이에요?");
+        int age = 0;
+        age = scanner.nextInt();
+        System.out.print("나이는 "+ age +" 입니다.");
+
     }
 }
